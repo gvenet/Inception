dcef346acfe7f52f2da8e3096d4f48cb23174bcf
IMac <imac@imac-de-imac.home>
2022-01-19 12:43:52 +0100

    maj makefile

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wp core download
-wp core config --dbname=wordpress --dbuser=gvenet --dbpass=root --dbprefix=wp_ --dbhost=mariadb
+wp core config --dbname=wordpress --dbuser=gvenet --dbpass=root --dbprefix=wp_ --dbhost=mariadb:3306
 wp core install --title="lololito" --url="totolezigoto.com" --admin_user="gvenet" --admin_password="root" --admin_email="tchuin@tourmentine.com"
 
 /usr/sbin/php-fpm7
