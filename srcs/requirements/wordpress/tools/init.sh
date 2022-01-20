7fba78efe90b3f170f2cf2de0a3e4e42d282b639
Guillaume Venet <gvenet@z4r8p1.42lyon.fr>
2022-01-20 18:09:35 +0100

    reste config db a mettre au propre avec un fichier .sql

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -2,6 +2,5 @@
 
 wp core download
 wp core config --dbname=wordpress --dbuser=gvenet --dbpass=root --dbprefix=wp_ --dbhost=mariadb:3306
-wp core install --title="lololito" --url="totolezigoto.com" --admin_user="gvenet" --admin_password="root" --admin_email="tchuin@tourmentine.com"
 
 /usr/sbin/php-fpm7
