ec8e641543f31cf6ee597e616b5b07839e0c41d4
Guillaume Venet <gvenet@z3r8p3.42lyon.fr>
2022-01-27 17:23:58 +0100

    reste Makefile a deplacer

--- srcs/requirements/mariadb/tools/mySqlScript.sql
+++
@@ -1,7 +0,0 @@
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
-CREATE DATABASE wordpress;
-CREATE USER 'gvenet'@'%' IDENTIFIED BY 'root';
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON wordpress.* TO 'gvenet'@'%';
-FLUSH PRIVILEGES;
-QUIT
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 wp core download
-wp core config --dbname=wordpress --dbuser=gvenet --dbpass=root --dbprefix=wp_ --dbhost=mariadb:3306
+
+wp core config \
+	--dbname=$WP_DATABASE_NAME \
+	--dbuser=$WP_DATABASE_USR \
+	--dbpass=$WP_DATABASE_PWD \
+	--dbprefix=wp_ \
+	--dbhost=$MYSQL_HOST:3306
+
+wp core install \
+	--url=localhost \
+	--title=inception \
+	--admin_user=$WP_ADMIN_USR \
+	--admin_password=$WP_ADMIN_PWD \
+	--admin_email=$WP_ADMIN_EMAIL
 
 /usr/sbin/php-fpm7
